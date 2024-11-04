4edee19316e4dfce146754df862e1d5e08fc6237
Alena <alenashcherb@gmail.com>
2024-11-04 12:01:46 +0300

    fixed func for very small number and large numbers

--- src/js/calculator/utils.js
+++ src/js/calculator/utils.js
@@ -1,8 +1,21 @@
 export function floatNumber(number) {
+  if (number === 0) return 0;
+
   let [integerPart] = number.toString().split('.');
-  if (integerPart.length > 10) {
+
+  // Condition for numbers with a large exponent or very small numbers
+  if (integerPart.length > 10 || Math.abs(number) < 1e-10) {
     return number.toExponential(10).replace(/\.?0+e/, 'e');
-  } else {
-    return +number.toFixed(10).replace(/\.?0+$/, '');
   }
+
+  //Processing of numbers with exponent > 10
+  let exponentialForm = number.toExponential();
+  let exponent = parseInt(exponentialForm.split('e')[1], 10);
+
+  if (exponent > 10) {
+    return number.toExponential(10).replace(/\.?0+e/, 'e');
+  }
+
+  // Normal rounding to 10 digits if the number fits in size
+  return +number.toFixed(10).replace(/\.?0+$/, '');
 }
